afef6d6b7b66362eaf9ff4202754106e8cddf8ee
Daniel Falster <daniel.falster@mq.edu.au>
2015-04-10 14:15:52 +1000

    Extend eample of mclapply to capture  and save output. #18 #28

--- scripts/example_mclapply.R
+++ scripts/example_mclapply.R
@@ -1,7 +1,7 @@
 
 # Illustartes one technique for running series of mdoels via mclapply
 
-options(mc.cores = 32L)
+options(mc.cores = 4L)
 library(parallel)
 
 remake::create_bindings()
@@ -15,13 +15,19 @@ all_pars_df <- expand.grid(iter = 10, stringsAsFactors = FALSE,
 							effect=c("constant", "trait", "species", "trait_species"),
 							data="stan_data")
 
+all_pars_df$name = sprintf("ignore/trial/%d", seq_len(nrow(all_pars_df)))
+
 ## turn that into a list
 all_pars <- split(all_pars_df, rownames(all_pars_df))
 
 ## function to run model given set of pars
 run_model <- function(pars) {
 	chunks <- str_eval(sprintf("get_chunks_model%d_%s()", pars$model, pars$effect ))
-	run_single_stan_chain(make_stan_model(chunks), get(pars$data), chain_id=pars$chain, iter=pars$iter)
+
+	capture_output2(
+		run_single_stan_chain(make_stan_model(chunks), get(pars$data), chain_id=pars$chain, iter=pars$iter),
+		name = pars$name, divert_messages=TRUE)
+
 }
 
-out <- mclapply(all_pars, run_model)
+out <- mclapply(all_pars[1:4], run_model)
