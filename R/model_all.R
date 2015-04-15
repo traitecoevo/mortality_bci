4383b6ab41f39dd56fe05c1da0db2e4b0fde747e
James Camac <james.camac@gmail.com>
2015-04-15 13:28:06 +1000

    Added trait models

--- R/model_all.R
+++ R/model_all.R
@@ -4,6 +4,9 @@ get_chunks <- function(model, effect) {
   models$constant <- list(get_chunks_model1_constant,
                           get_chunks_model2_constant,
                           get_chunks_model3_constant)
+  models$trait <- list(get_chunks_model1_trait,
+                          get_chunks_model2_trait,
+                          get_chunks_model3_trait)
   models$species <- list(get_chunks_model1_species,
                          get_chunks_model2_species,
                          get_chunks_model3_species)
