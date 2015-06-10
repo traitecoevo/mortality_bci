d7cc542ead28008ac02dc25d7a03a98112228220
James Camac <james.camac@gmail.com>
2015-06-10 17:12:48 +1000

    Changed function names

--- R/run_models.R
+++ R/run_models.R
@@ -6,7 +6,7 @@
 ## meet part way with rrqueue.
 #remake::make("data")
 
-packages <- c("rstan", "plyr", "parallel")
+packages <- c("rstan","plyr","parallel")
 sources <- c("R/model.R",
              "R/task_compiler.R",
              "R/stan_functions.R",
@@ -28,12 +28,12 @@ run_rho_combination(iter = 10, growth_measure = 'dbh_dt')
 
 
 # Test on subset - growth combinations
-pars <- pars_growth(iter = 10)
-pars <- pars[c(1,4,120),]
-create_dirs(unique(dirname(pars$filename)))
-ret <- mclapply(df_to_list(pars), model_compiler)
+tasks <- tasks_growth(iter = 10)
+tasks <- tasks[c(1,4,120),]
+create_dirs(unique(dirname(tasks$filename)))
+ret <- mclapply(df_to_list(tasks), model_compiler)
 # Test on subset - rho combinations
-pars <- pars_rho_combos(iter = 10,growth_measure = 'dbh_dt')
-pars <- pars[c(1,4,215,232),]
-create_dirs(unique(dirname(pars$filename)))
-ret <- mclapply(df_to_list(pars), model_compiler)
+tasks <- tasks_rho_combos(iter = 10,growth_measure = 'dbh_dt')
+tasks <- tasks[c(1,4,215,232),]
+create_dirs(unique(dirname(tasks$filename)))
+ret <- mclapply(df_to_list(tasks), model_compiler)
