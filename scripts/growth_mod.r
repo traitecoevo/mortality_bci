442a46ef88de57de8c63ffbc18edf7233fdff512
James Camac <james.camac@gmail.com>
2015-06-18 22:41:36 +1000

    Improve sampling efficiency

--- scripts/growth_mod.r
+++ scripts/growth_mod.r
@@ -1,7 +1,7 @@
 #Before running code first run make() if data folds have not yet been compiled.
 
 data <-readRDS('export/bci_data_1.rds')
-data$train <- data$train[1:5000,]
+data$train <- data$train[1:100,]
 prep_data_for_stan <- function(data, growth_measure) {
   
   list(
@@ -12,6 +12,7 @@ prep_data_for_stan <- function(data, growth_measure) {
     obs_gr =  data$train[[growth_measure]],
     dbh = data$train$dbh)
 }
+
 
 stan_data <- prep_data_for_stan(data, 'dbh_dt')
 model <- '
@@ -25,24 +26,38 @@ model <- '
   }
 
 parameters { 
-  real log_pred[n_obs];
-  real<lower=0> true_gr[n_obs];
   real<lower=0> true_gr_sigma[n_spp];
   real log_pred_mu;
   real<lower=0> log_pred_gr_sigma;
+  real raw_true_gr[n_obs];
+  real raw_log_pred[n_obs];
 }
-
-model {  
+transformed parameters {
+real<lower=0> true_gr[n_obs];
+real log_pred[n_obs];
+for(i in 1:n_obs) {
+log_pred[i] <- raw_log_pred[i] * log_pred_gr_sigma + log_pred_mu;
+true_gr[i] <- exp(raw_true_gr[i] * true_gr_sigma[spp[i]] + log_pred[i]);
+}
+}
+model {
   log_pred_gr_sigma ~ cauchy(0, 2.5);
   for (s in 1:n_spp) {
     true_gr_sigma[s] ~ cauchy(0, 2.5);
   } 
   for (i in 1:n_obs) { # growth rate observation error outlined in Ruger et al 2011.
-    log_pred[i] ~ normal(log_pred_mu, log_pred_gr_sigma);
-    true_gr[i] ~ lognormal(log_pred[i], true_gr_sigma[spp[i]]);
-    increment_log_prob(log_sum_exp(log(1 - 0.0276) + normal_log(obs_gr[i], true_gr[i], (0.000927 + 0.0000038 * dbh[i])/census_length[i]), // size dependent error
-                                   log(0.0276) + normal_log(obs_gr[i],true_gr[i], (0.0256/census_length[i])))); // size independent error
+    raw_log_pred[i] ~ normal(0,1);
+    raw_true_gr[i] ~ normal(0,1);
+    increment_log_prob(log_sum_exp(log(0.9724) + normal_log(obs_gr[i], true_gr[i], (0.000927 + 0.0000038 * dbh[i])/census_length[i]),
+                                   log(0.0276) + normal_log(obs_gr[i],true_gr[i], (0.0256/census_length[i]))));
   }
 }'
 
-gr_mod <- stan(model_code = model, data = stan_data, pars = c('true_gr'), chains = 3, iter = 2000, control=list(adapt_delta=0.9))
+gr_mod3 <- stan(model_code = model, data = stan_data, pars = c('true_gr','true_gr_sigma','log_pred_mu', 'raw_true_gr'), chains = 3, iter = 2000, control=list(adapt_delta=0.9))
+gr_mod2 <- sflist2stanfit(sflist <-mclapply(1:3, mc.cores=3, function(i) stan(model_code = model, 
+                                           data = stan_data, 
+                                           pars = c('true_gr'), chains = 1, chain_id=i,
+                                           iter = 2000, control=list(adapt_delta=0.9))))
+         
+x <- summary(gr_mod3, 'true_gr')$summary
+plot(stan_data$obs_gr ~ as.vector(x[,'mean']))
