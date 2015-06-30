a5d3ebea067bd433a06165a4971e5a23e6b34b98
James Camac <james.camac@gmail.com>
2015-06-30 17:20:58 +1000

    Altered task functions so that rho_combination task function firsts checks to see if models have already been run in growth comparisons. If so copies these over to the appropriate folder and renames them.

--- R/task_compiler.R
+++ R/task_compiler.R
@@ -1,8 +1,7 @@
-# What growth rate best predicts mortality using full model
-tasks_2_run <- function(iter, name, growth_measure, rho_combo) {
+tasks_2_run <- function(iter, name, growth_measure, rho_combo, tasks_run=tasks_run) {
   n_kfolds <- 10
   n_chains <- 3
- 
+  
   ret <- expand.grid(experiment=name,
                      iter=iter,
                      chain=seq_len(n_chains),
@@ -14,6 +13,17 @@ tasks_2_run <- function(iter, name, growth_measure, rho_combo) {
   ret$jobid <- seq_len(nrow(ret))
   ret$filename <- sprintf("results/%s/%d.rds", name, ret$jobid)
   ret$fold_data <- sprintf("export/bci_data_%s.rds", ret$kfold)
+  create_dirs(unique(dirname(ret$filename)))
+  if(isTRUE(tasks_run)) {
+    i <- match(do.call(paste, ret[, c('chain', 'growth_measure', 'rho_combo', 'kfold')]),
+               do.call(paste, tasks[, c('chain', 'growth_measure', 'rho_combo', 'kfold')]))
+    ff <- file.copy(tasks$filename[na.omit(i)], ret$filename[which(!is.na(i))])
+    if(!all(ff)) {
+      warning(sprintf('Some previously run model outputs failed to copy:\n%s',
+                      paste(tasks$filename[na.omit(i)][!ff], collapse='\n')))
+    }
+    return(ret[is.na(i), ]  )
+  }
   ret
 }
 
@@ -26,31 +36,31 @@ tasks_growth <- function(iter=2000, name = 'growth_comparisons') {
                         "dbh_dt_rel", 
                         "basal_area_dt",
                         "basal_area_dt_rel"),
-    rho_combo="abc")
+    rho_combo="abc",
+    tasks_run=FALSE)
 }
 
 # Based on best growth rate model which combination of rho effects is most parsimonous
-tasks_rho_combos <- function(iter=2000, growth_measure, name="rho_combinations") {
+tasks_rho_combos <- function(iter=2000, growth_measure, name="rho_combinations", tasks_run) {
   rho_combo <- expand.grid(a=c('','a'), b=c('','b'), c=c('','c'), stringsAsFactors = FALSE)
   rho_combo <- sapply(split(rho_combo, seq_len(nrow(rho_combo))), function(x) paste0(x, collapse=''))
   tasks_2_run(iter, 
        name=name, 
        growth_measure =  growth_measure,
-       rho_combo=rho_combo)
+       rho_combo=rho_combo,
+       tasks_run=tasks_run)
 }
 
 # Launching growth comparison analysis
 run_growth_comparison <- function(iter=2000) {
   tasks <- tasks_growth(iter = iter, name="growth_comparison")
-  create_dirs(unique(dirname(tasks$filename)))
   ret <- mclapply(df_to_list(tasks), model_compiler)
 }
 
 # Launching rho combination analysis
 
 run_rho_combination <- function(iter=2000, growth_measure) {
-  tasks <- tasks_rho_combos(iter = iter, growth_measure = growth_measure)
-  create_dirs(unique(dirname(tasks$filename)))
+  tasks <- tasks_rho_combos(iter = iter, growth_measure = growth_measure, tasks_run=TRUE)
 
   # Check is any of tasks already run in prev experiment, if so copy across and give correct id
   tasks_done <- tasks_growth(iter = iter)
