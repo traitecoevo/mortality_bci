cbee70821fd337802d450f07033f622e2ea24e67
James Camac <james.camac@gmail.com>
2015-08-25 09:31:29 +1000

    Model appears to work with no divergent issues when we estimate mu census

--- R/model.R
+++ R/model.R
@@ -6,9 +6,9 @@ get_model_chunks <- function(tasks) {
   }
 
   list(
-    pars = c("mu_log_a0","sigma_log_a0","a0","sigma_log_a1","a1",if("a" %in% rho_combo) "a2",
-             "mu_log_b0","sigma_log_b0","b0","sigma_log_b1","b1",if("b" %in% rho_combo) "b2",
-             "mu_log_c0","sigma_log_c0","c0","sigma_log_c1","c1",if("c" %in% rho_combo) "c2",
+    pars = c("mu_log_a0","sigma_log_a0","a0","mu_log_a1","sigma_log_a1","a1",if("a" %in% rho_combo) "a2",
+             "mu_log_b0","sigma_log_b0","b0","mu_log_b1","sigma_log_b1","b1",if("b" %in% rho_combo) "b2",
+             "mu_log_c0","sigma_log_c0","c0","mu_log_c1","sigma_log_c1","c1",if("c" %in% rho_combo) "c2",
              "sum_log_lik_fit","sum_log_lik_heldout"),
     parameters = sprintf("
       // Mortality model parameters
@@ -17,6 +17,7 @@ get_model_chunks <- function(tasks) {
       real<lower=0> sigma_log_a0;
 
       real raw_log_a1[n_census];
+      real mu_log_a1;
       real<lower=0> sigma_log_a1;
 
       real raw_log_b0[n_spp];
@@ -24,6 +25,7 @@ get_model_chunks <- function(tasks) {
       real<lower=0> sigma_log_b0;
 
       real raw_log_b1[n_census];
+      real mu_log_b1;
       real<lower=0> sigma_log_b1;
 
       real raw_log_c0[n_spp];
@@ -31,6 +33,7 @@ get_model_chunks <- function(tasks) {
       real<lower=0> sigma_log_c0;
 
       real raw_log_c1[n_census];
+      real mu_log_c1;
       real<lower=0> sigma_log_c1;
 
       %s
@@ -52,7 +55,7 @@ get_model_chunks <- function(tasks) {
     real gamma;
     real c0[n_spp];
     real c1[n_census];
-
+  
     real cumulative_hazard;
 
     // Calculating species random effects
@@ -64,15 +67,15 @@ get_model_chunks <- function(tasks) {
 
     // Calculating census period random effects
     for (t in 1:n_census) {
-      a1[t] <- exp(raw_log_a1[t] * sigma_log_a1); // e.g. implies lognormal(0, sigma_log_a1)
-      b1[t] <- exp(raw_log_b1[t] * sigma_log_b1);
-      c1[t] <- exp(raw_log_c1[t] * sigma_log_c1);
+      a1[t] <- exp(raw_log_a1[t] * sigma_log_a1 + mu_log_a1); // e.g. implies lognormal(mu_log_a1, sigma_log_a1)
+      b1[t] <- exp(raw_log_b1[t] * sigma_log_b1 + mu_log_b1);
+      c1[t] <- exp(raw_log_c1[t] * sigma_log_c1 + mu_log_c1);
     }
 
     for (i in 1:n_obs) {
       // Calculating mortality parameters
-      alpha <- a0[spp[i]] * a1[census[i]]%s;
-      beta <- b0[spp[i]] * b1[census[i]]%s;
+      alpha <- a0[spp[i]] * a1[census[i]]%s; 
+      beta <- b0[spp[i]] * b1[census[i]]%s; 
       gamma <- c0[spp[i]] * c1[census[i]]%s;
 
     // Likelihood for hazard model
@@ -100,14 +103,17 @@ get_model_chunks <- function(tasks) {
     mu_log_c0 ~ normal(-3.86, 0.2);
     sigma_log_c0 ~ cauchy(0, 2.5);
 
-    raw_log_a1 ~ normal(0,1);
-    sigma_log_a1 ~ cauchy(0, 2.5);
+     raw_log_a1 ~ normal(0,1);
+     mu_log_a1 ~ normal(0, 5);
+     sigma_log_a1 ~ cauchy(0, 2.5);
 
-    raw_log_b1 ~ normal(0, 1);
-    sigma_log_b1 ~ cauchy(0, 2.5);
+     raw_log_b1 ~ normal(0, 1);
+     mu_log_b1 ~ normal(0, 5);
+     sigma_log_b1 ~ cauchy(0, 2.5);
 
-    raw_log_c1 ~ normal(0, 1);
-    sigma_log_c1 ~ cauchy(0, 2.5);
+     raw_log_c1 ~ normal(0,1);
+     mu_log_c1 ~ normal(0,5);
+     sigma_log_c1 ~ cauchy(0, 2.5);
 
     %s
     %s
@@ -132,6 +138,7 @@ get_model_chunks <- function(tasks) {
     real alpha_fit;
     real beta_fit;
     real gamma_fit;
+
     real cumulative_hazard_fit;
     real log_lik_fit;
     real sum_log_lik_fit;
@@ -140,6 +147,7 @@ get_model_chunks <- function(tasks) {
     real alpha_heldout;
     real beta_heldout;
     real gamma_heldout;
+
     real cumulative_hazard_heldout;
     real log_lik_heldout[n_obs_heldout];
     real sum_log_lik_heldout;
@@ -155,17 +163,17 @@ get_model_chunks <- function(tasks) {
       c0[s] <- exp(raw_log_c0[s] * sigma_log_c0 + mu_log_c0);
     }
 
-    // recalulate species random effects
-    for (t in 1:n_census) {
-      a1[t] <- exp(raw_log_a1[t] * sigma_log_a1);
-      b1[t] <- exp(raw_log_b1[t] * sigma_log_b1);
-      c1[t] <- exp(raw_log_c1[t] * sigma_log_c1);
-    }
+    // recalulate census random effects
+     for (t in 1:n_census) {
+       a1[t] <- exp(raw_log_a1[t] * sigma_log_a1 + mu_log_a1);
+       b1[t] <- exp(raw_log_b1[t] * sigma_log_b1 + mu_log_b1);
+       c1[t] <- exp(raw_log_c1[t] * sigma_log_c1 + mu_log_c1);
+     }
 
     // log likelihood for fitted model
     for (i in 1:n_obs) {
-      alpha_fit <- a0[spp[i]] * a1[census[i]]%s;
-      beta_fit <- b0[spp[i]] * b1[census[i]]%s;
+      alpha_fit <- a0[spp[i]] * a1[census[i]]%s; 
+      beta_fit <- b0[spp[i]] * b1[census[i]]%s; 
       gamma_fit <- c0[spp[i]] * c1[census[i]]%s;
 
       cumulative_hazard_fit <- -census_length[i] * (alpha_fit * exp(-beta_fit * growth_dt[i]) + gamma_fit);
@@ -181,8 +189,8 @@ get_model_chunks <- function(tasks) {
 
     // log likelihood for held out data
     for (j in 1:n_obs_heldout) {
-      alpha_heldout <- a0[spp_heldout[j]] * a1[census_heldout[j]]%s;
-      beta_heldout <- b0[spp_heldout[j]] * b1[census_heldout[j]]%s;
+      alpha_heldout <- a0[spp_heldout[j]] * a1[census_heldout[j]]%s; 
+      beta_heldout <- b0[spp_heldout[j]] * b1[census_heldout[j]]%s; 
       gamma_heldout <- c0[spp_heldout[j]] * c1[census_heldout[j]]%s;
 
       cumulative_hazard_heldout <- -census_length_heldout[j] * (alpha_heldout * exp(-beta_heldout * growth_dt_heldout[j]) + gamma_heldout);
