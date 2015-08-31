410aca280e8b13f05a58814a32ebeea7243150ca
James Camac <james.camac@gmail.com>
2015-08-31 16:39:22 +1000

    Added observed growth measures

--- R/bci_data_processing.R
+++ R/bci_data_processing.R
@@ -247,8 +247,10 @@ add_true_growth <- function(data) {
   true_dbh_mod <- readRDS('results/true_dbh/true_dbh_model.rds')
   data$true_dbh1 <- as.vector(get_posterior_mean(true_dbh_mod, 'true_dbh1')[,'mean-all chains'])
   data$true_dbh2 <- as.vector(get_posterior_mean(true_dbh_mod, 'true_dbh2')[,'mean-all chains'])
+  data$obs_dbh_dt <- (data$dbh - data$dbh_prev)/data$census_interval
   data$true_dbh_dt <- (data$true_dbh2 - data$true_dbh1)/data$census_interval
   data$true_dbh_dt_rel <- (log(data$true_dbh2) - log(data$true_dbh1))/data$census_interval
+  data$obs_basal_area_dt <- ((0.25 * pi * data$dbh^2) -  (0.25 * pi * data$dbh_prev^2))/data$census_interval
   data$true_basal_area_dt <- ((0.25 * pi * data$true_dbh2^2) -  (0.25 * pi * data$true_dbh1^2))/data$census_interval
   data$true_basal_area_dt_rel <- (log(0.25 * pi * data$true_dbh2^2) -  log(0.25 * pi * data$true_dbh1^2))/data$census_interval
   data
