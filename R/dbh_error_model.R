8e239106663af9b5237356958e5d0bdf865c8089
James Camac <james.camac@gmail.com>
2015-09-01 16:43:47 +1000

    Edited script to work with remake

--- R/dbh_error_model.R
+++ R/dbh_error_model.R
@@ -1,17 +1,4 @@
-run_dbh_error_model <- function(iter=1000) {
-  if (file.exists('results/dbh_error/dbh_error_model.rds')){
-    stop('dbh error model has already been run')
-    }
-dat <- readRDS('data/dbh_error_data.rds')
-#supplied directly from Ruger. 
-# Need to do a bit of cleaning & MYSQL work if we wish to use the open source.
-
-dat$dbh1 <- dat$dbh1/10 # convert mm to cm
-dat$dbh2 <- dat$dbh2/10 # convert mm to cm 
-dat$discrep <- dat$dbh1 - dat$dbh2 
-
-create_dirs('results/dbh_error')
-
+run_dbh_error_model <- function(dat) {
 stan_data <- list(
   n_obs = nrow(dat),
   discrep = dat$discrep)
@@ -35,10 +22,8 @@ fit <-stan(model_code = dbh_error_model,
            data= stan_data,
            pars = c("sigma"), 
            chains = 3, 
-           iter = iter, 
+           iter = 1000, 
            control=list(stepsize=0.01, adapt_delta=0.99),
            refresh=1)
-
-#fit@.MISC <- emptyenv()
-saveRDS(fit, 'results/dbh_error/dbh_error_model.rds')
+fit
 }
