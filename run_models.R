67d29b2ae2ce3a67b21f033f13e8d46fc0fbaf09
James Camac <james.camac@gmail.com>
2015-09-02 09:57:54 +1000

    Removed functions to run mortality analysis. Need to rework code to work with clusterous.

--- run_models.R
+++ run_models.R
@@ -1,7 +1,9 @@
+# To run the mortality analysis it first requires the data to be downloaded, cleaned and processed for use with stan.
+# To do this run remake::('export_1')
+# Note this will take approximately 10 hours as it needs to estimate true growth for every observation in the dataset.
+
 packages <- c("plyr","parallel","remake","rstan")
-sources <- c("R/dbh_error_model.r",
-             "R/true_dbh_model.r",
-             "R/model.R",
+sources <- c("R/model.R",
              "R/task_compiler.R",
              "R/stan_functions.R",
              "R/utils.R")
@@ -14,27 +16,6 @@ for (s in sources) {
 }
 
 
-# FIRST run make stage 1:
-#make() not functional yet
-
-# NOTE the following models run 100's of chains
-# each taking X hours. These models are best run on a cluster.
-
-# 3) Launching growth comparison analysis
-run_growth_comparison(iter = 1000)
-
-# 4) Launching rho combination analysis (only run once best growth rate has been determined)
-run_rho_combination(iter = 1000, growth_measure = 'dbh_dt')
-
-
-# Remove this code once testing is complete
-# Test on subset - growth combinations
-tasks <- tasks_growth(iter = 2000,name = 'final_test')
-tasks <- tasks[c(1:12),]
-create_dirs(unique(dirname(tasks$filename)))
-ret <- mclapply(df_to_list(tasks), model_compiler, mc.cores=12)
-# Test on subset - rho combinations
-tasks <- tasks_rho_combos(iter = 10,growth_measure = 'dbh_dt')
-tasks <- tasks[c(1,4,215,232),]
-create_dirs(unique(dirname(tasks$filename)))
+# Launching growth comparison analysis - need to replace mclapply
+tasks <- tasks_growth(iter = 1000, name="growth_comparison")
 ret <- mclapply(df_to_list(tasks), model_compiler)
