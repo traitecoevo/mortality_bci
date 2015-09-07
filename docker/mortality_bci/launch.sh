57815e4d376901467fbcec1ad3357ecdd1a2c650
Daniel Falster <daniel.falster@mq.edu.au>
2015-09-07 17:20:09 +1000

    Simplify rstan installtion
    - Can now install via cran so no longer require separate module
    - Still requires additonal memory for build to complete
    - built using dockertest running via docker-machine (see
    traitecoevo/dockertest/commit/62fae6487ae00ec12dfdf7594fcfd242714f9877)

--- docker/mortality_bci/launch.sh
+++ docker/mortality_bci/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-$(boot2docker shellinit 2> /dev/null)
+eval '$(docker-machine env default)'
 docker run -v /Users/dfalster/Dropbox/_research/SIEF/mortality_bci:/src -it traitecoevo/mortality_bci $*
