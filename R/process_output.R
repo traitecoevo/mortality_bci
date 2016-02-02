c25e1181b542f6ab5917bbe1ac331c35f80b8eba
James Camac <james.camac@gmail.com>
2016-02-02 23:02:22 +1100

    Fixed log likelihood summarising function and added some plot functions for preliminary results

--- R/process_output.R
+++ R/process_output.R
@@ -82,24 +82,93 @@ extract_loglik_samples <- function(analysis) {
   res <- plyr::ldply(lapply(samples, function(x) {
     gather(data.frame(x),'likelihood','estimate')}), .id='modelid')
   
-  left_join(info, res, 'modelid') %>%
-    select(-modelid)
+  left_join(info, res, 'modelid')
 }
 
 # Extract log likelihood samples for multiple analyses.
 extract_multi_analysis_loglik_samples <- function(list_of_analyses){
   samples <- lapply(list_of_analyses, extract_loglik_samples)
-  plyr::ldply(samples, .id='model') %>%
-    select(-model)
+  plyr::ldply(samples, .id='model')
 }
 # Summarise log likelihood samples
 summarise_loglik_samples <- function(samples) {
   samples %>%
-    group_by(analysis,growth_measure,rho_combo, likelihood) %>%
-    summarise(mean = mean(estimate),
-              `2.5%` = quantile(estimate, 0.025),
-              `10%` = quantile(estimate, 0.1),
-              `50%` = quantile(estimate, 0.5),
-              `90%` = quantile(estimate, 0.9),
-              `97.5%` = quantile(estimate, 0.975))
+    group_by(modelid, model, analysis,growth_measure,rho_combo,kfold, likelihood) %>%
+    summarise(klpd = mean(log_sum_exp(estimate))) %>%
+    ungroup() %>%
+    group_by(model, analysis, growth_measure, rho_combo, likelihood) %>%
+    summarise(elpd = mean(klpd),
+              st_err = sd(klpd)/sqrt(n())) %>%
+    mutate(ci = 1.96 * st_err,
+           upper_ci = elpd + ci,
+           lower_ci = elpd - ci) %>%
+    ungroup()
 }
+
+coefficent_plot_theme <- function() {
+  theme_classic() + theme(axis.title.x = element_text(face = "bold", size = 12),
+                          axis.title.y = element_blank(),
+                          axis.text.x = element_text(size = 10),
+                          axis.text.y = element_text(size = 12),
+                          plot.title = element_text(size = 18),
+                          title = element_text(face = "bold"),
+                          panel.margin = unit(4,"mm"))
+}
+
+test2 <-subset(test, model %in% c('rho_combinations','no_gamma_model', 'null_model') & likelihood =='sum_log_lik_heldout' & rho_combo=='' & growth_measure=='true_dbh_dt')
+
+plot_functional_form_comparison <- function(log_likelihood_summary) {
+  dat <- filter(log_likelihood_summary, model %in% c('null_model','no_gamma_model', 
+                                                     'rho_combinations', 'null_model_random_effects', 
+                                                     'no_gamma_model_random_effects', 'growth_comparison') & 
+                  likelihood =='sum_log_lik_heldout' & 
+                  rho_combo=='' & 
+                  growth_measure=='true_dbh_dt') %>%
+    mutate(model = factor(model, levels=c('null_model','no_gamma_model', 
+                                          'rho_combinations', 'null_model_random_effects', 
+                                          'no_gamma_model_random_effects', 'growth_comparison'))) %>%
+    mutate(model = factor(model, labels = c( "Baseline hazard","Growth hazard","Baseline & growth hazard",
+                                             "Baseline hazard (spp)","Growth hazard (spp)",
+                                             "Baseline & growth hazard (spp)"))) %>%
+    arrange(model)
+  
+  ggplot(dat, aes(x = elpd,y = model)) + 
+    geom_segment(aes(x=lower_ci,y=model, xend=upper_ci, yend=model), size=0.5)+
+    geom_point(aes(x=elpd, y=model), size =3) +
+    xlab('Log likelihood') +
+    scale_x_continuous(breaks= scales::pretty_breaks(6)) +
+    coefficent_plot_theme()
+}
+
+plot_growth_comparison <- function(log_likelihood_summary) {
+  dat <- filter(log_likelihood_summary, model =='growth_comparison' & 
+                  likelihood =='sum_log_lik_heldout' & 
+                  rho_combo=='') %>%
+    mutate(growth_measure = factor(growth_measure, levels=c("true_basal_area_dt","true_dbh_dt"))) %>%
+    mutate(growth_measure = factor(growth_measure, labels = c("Area growth","Increment growth"))) %>%
+    arrange(growth_measure)
+  
+  ggplot(dat, aes(x = elpd,y = growth_measure)) + 
+    geom_segment(aes(x=lower_ci,y=growth_measure, xend=upper_ci, yend=growth_measure), size=0.5)+
+    geom_point(aes(x=elpd, y=growth_measure), size =3) +
+    xlab('Log likelihood') +
+    scale_x_continuous(breaks= scales::pretty_breaks(6)) +
+    coefficent_plot_theme()
+}
+
+
+plot_rho_comparison <- function(log_likelihood_summary) {
+  dat <- filter(log_likelihood_summary, model =='rho_combinations' & 
+                  likelihood =='sum_log_lik_heldout') %>%
+    mutate(rho_combo = factor(rho_combo, levels=c("abc","bc","ac","ab","c","b","a",""))) %>%
+    mutate(rho_combo = factor(rho_combo, labels = c("all parameters","beta & gamma","alpha & gamma",'alpha & beta', "gamma", "beta","alpha","none"))) %>%
+    arrange(rho_combo)
+  
+  ggplot(dat, aes(x = elpd,y = rho_combo)) + 
+    geom_segment(aes(x=lower_ci,y=rho_combo, xend=upper_ci, yend=rho_combo), size=0.5)+
+    geom_point(aes(x=elpd, y=rho_combo), size =3) +
+    xlab('Log likelihood') +
+    scale_x_continuous(breaks= scales::pretty_breaks(6)) +
+    coefficent_plot_theme()
+}
+
