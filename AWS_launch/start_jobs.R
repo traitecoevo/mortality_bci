a83e848588dea79247c89d942e6dbbbe8bc084ba
James Camac <james.camac@gmail.com>
2016-03-30 10:19:17 +1100

    Updated script and number of iterations

--- AWS_launch/start_jobs.R
+++ AWS_launch/start_jobs.R
@@ -11,19 +11,19 @@ sources <- c("R/model.R",
 obj <- queue("rrq", redis_host="localhost", packages=packages, sources=sources,  redis_port = 31379)
 
 # PHASE 1: BEST GROWTH & FUNCTIONAL FORM
-func_growth_tasks <- tasks_2_run(comparison = 'function_growth_comparison',iter = 50, 
+func_growth_tasks <- tasks_2_run(comparison = 'function_growth_comparison',iter = 4000, 
                      path="/home/data")
 res <- enqueue_bulk(func_growth_tasks, model_compiler, obj, progress_bar = TRUE)
 
 # PHASE 2: BEST GROWTH & FUNCTIONAL FORM
-species_re_tasks <- tasks_2_run(comparison = 'species_random_effects',iter = 20, 
+species_re_tasks <- tasks_2_run(comparison = 'species_random_effects',iter = 4000, 
                      path="/home/data")
 res <- enqueue_bulk(species_re_tasks, model_compiler, obj, progress_bar = TRUE)
 
 # PHASE 3:  BEST RHO COMBINATION
-null_re_tasks <- tasks_2_run(comparison = 'rho_combinations',iter = 20, 
+rho_combos <- tasks_2_run(comparison = 'rho_combinations',iter = 4000, 
                      path="/home/data")
-res <- enqueue_bulk(null_re_tasks, model_compiler, obj, progress_bar = TRUE)
+res <- enqueue_bulk(rho_combos, model_compiler, obj, progress_bar = TRUE)
 
 
 
