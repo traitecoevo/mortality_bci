67221303519e17bbf65ba50659d02ddf1cc4e8b3
James Camac <james.camac@gmail.com>
2016-05-17 16:37:07 +1000

    Major changes here. Built wrapper functions to better integrate workflows for both full data models and crossval

--- R/stan_functions.R
+++ R/stan_functions.R
@@ -1,28 +1,28 @@
-# Generic task builder function for clusterous
-tasks_2_run <- function(comparison,iter=4000,path='.') {
-  if(!comparison %in% c("function_growth_comparison","species_random_effects","rho_combinations")) {
-    stop('comparison can only be one of the following: 
-                      "function_growth_comparison","species_random_effects","rho_combinations"')
-  }
+# Kfold tasks
+kfold_tasks <- function(comparison,iter=4000,path='.') {
   n_kfolds = 10
   n_chains = 3
-  if(comparison=="function_growth_comparison") {
-    growth_measure <- c("true_dbh_dt",'true_basal_area_dt')
-    rho_combo <- "none"
-    model  <- c("base_hazard","growth_hazard","base_growth_hazard")
-    } 
-  if(comparison=="species_random_effects") {
-    growth_measure <- c("true_dbh_dt")
-    rho_combo <- "none"
-    model  <- "base_growth_hazard_re"
-    }
-  if(comparison =="rho_combinations") {
-  growth_measure <- 'true_dbh_dt'
-  rho_combo <- expand.grid(a=c('','a'), b=c('','b'), c=c('','c'), stringsAsFactors = FALSE)
-  rho_combo <- sapply(split(rho_combo, seq_len(nrow(rho_combo))), function(x) paste0(x, collapse=''))
-  rho_combo[rho_combo==''] <- "none"
-  model <- "base_growth_hazard"
-  }
+  switch(comparison,
+         "function_growth_comparison" = {
+           growth_measure <- c("true_dbh_dt",'true_basal_area_dt')
+           rho_combo <- "none"
+           model  <- c("base_hazard","growth_hazard","base_growth_hazard")
+         }, 
+         "species_random_effects" = {
+           growth_measure <- c("true_dbh_dt")
+           rho_combo <- "none"
+           model  <- "base_growth_hazard_re"
+         },
+         "rho_combinations" = {
+           growth_measure <- 'true_dbh_dt'
+           rho_combo <- expand.grid(a=c('','a'), b=c('','b'), c=c('','c'), stringsAsFactors = FALSE)
+           rho_combo <- sapply(split(rho_combo, seq_len(nrow(rho_combo))), function(x) paste0(x, collapse=''))
+           rho_combo[rho_combo==''] <- "none"
+           model <- "base_growth_hazard"
+         },
+         stop('comparison can only be one of the following: 
+                      "function_growth_comparison","species_random_effects","rho_combinations"')
+         )
   ret <- expand.grid(comparison=comparison,
                      model = model,
                      iter=iter,
@@ -32,7 +32,7 @@ tasks_2_run <- function(comparison,iter=4000,path='.') {
                      kfold=seq_len(n_kfolds),
                      stringsAsFactors=FALSE) %>%
     arrange(model, growth_measure)
-    
+  
   ret$modelid <- rep(1:nrow(unique(ret[,c('comparison','model','growth_measure','rho_combo','kfold')])),each = n_chains)
   ret <- ret %>%
     mutate(jobid = seq_len(n()),
@@ -40,30 +40,87 @@ tasks_2_run <- function(comparison,iter=4000,path='.') {
            fold_data = sprintf("%s/precompile/kfold_data/bci_data_%s.rds", path, kfold))
   return(ret)
 }
+
+# Full data tasks
+full_data_tasks <- function(model, iter=4000,path='.') {
+  n_chains = 3
+  switch(model,
+         "final_model" = {
+           rho_combo = "c"
+           comparison = 'final_model'
+         },
+         "final_base_growth_hazard_re" = {
+           rho_combo = "none"
+           comparison = "final_base_growth_hazard_re"
+         }, 
+         stop("Model must be either 'final_model' or 'final_base_growth_hazard_re'"))
+         
+         ret <- expand.grid(comparison=comparison,
+                            model=model,
+                            iter=iter,
+                            chain=seq_len(n_chains),
+                            growth_measure= "true_dbh_dt",
+                            rho_combo= rho_combo,
+                            kfold=0,
+                            stringsAsFactors=FALSE)
+         
+         ret$modelid <- rep(1:nrow(unique(ret[,c('comparison','model','growth_measure','rho_combo','kfold')])),each = n_chains)
+         ret <- ret %>%
+           mutate(jobid = seq_len(n()),
+                  filename = sprintf("%s/results/chain_fits/%s/%d.rds", path, model, jobid),
+                  fold_data = sprintf("%s/precompile/bci_data_full.rds", path))
+         return(ret)
+}
+
+# Wrapper function to allow for both full data and kfold tasks
+tasks_2_run <- function(comparison,iter=4000,path='.') {
+  if(comparison %in% c("final_model","final_base_growth_hazard_re")) {
+    full_data_tasks(comparison, iter, path)
+  } 
+  else {
+    kfold_tasks(comparison, iter, path)
+  }
+}
+
 # Compiles models for clusterous
 model_compiler <- function(task) {
   data <- readRDS(task$fold_data)
   dir.create(dirname(task$filename), FALSE, TRUE)
   model <- task$model
   ## Assemble the stan model:
-  if(model == "base_hazard") {
-    chunks <- get_model_chunks_base_haz(task)
-  }
-  if(model == "base_hazard_re") {
-    chunks <- get_model_chunks_base_haz_re(task)
-  }
-  if(model == "growth_hazard") {
-    chunks <- get_model_chunks_growth_haz(task)
-  }
-  if(model == "growth_hazard_re") {
-    chunks <- get_model_chunks_growth_haz_re(task)
-  }
-  if(model == "base_growth_hazard") {
-    chunks <- get_model_chunks_base_growth_haz(task)
-  }
-  if(model == "base_growth_hazard_re") {
-    chunks <- get_model_chunks_base_growth_haz_re(task)
-  }
+  switch(model, 
+         "base_hazard"= {
+           chunks <- get_model_chunks_base_haz(task)
+           chunks$crossval <- TRUE
+         },
+         "base_hazard_re"= {
+           chunks <- get_model_chunks_base_haz_re(task)
+           chunks$crossval <- TRUE
+         },
+         "growth_hazard"= {
+           chunks <- get_model_chunks_growth_haz(task)
+           chunks$crossval <- TRUE
+         },
+         "growth_hazard_re"= {
+           chunks <- get_model_chunks_growth_haz_re(task)
+           chunks$crossval <- TRUE
+         },
+         "base_growth_hazard"= {
+           chunks <- get_model_chunks_base_growth_haz(task)
+           chunks$crossval <- TRUE
+         },
+         "base_growth_hazard_re"= {
+           chunks <- get_model_chunks_base_growth_haz_re(task)
+           chunks$crossval <- TRUE
+         },
+         'final_model'= {
+           chunks <- get_final_model_chunks(task)
+           chunks$crossval <- FALSE
+         },
+         "final_base_growth_hazard_re"= {
+           chunks <- get_model_chunks_base_growth_haz_re(task, TRUE)
+           chunks$crossval <- FALSE
+         })
   model <- make_stan_model(chunks)
   
   filename <- precompile(task)
@@ -82,7 +139,7 @@ model_compiler <- function(task) {
 # Runs single chain
 run_single_stan_chain <- function(model, data, chain_id, iter=4000,
                                   sample_file=NA, diagnostic_file=NA) {
-  data_for_stan <- prep_data_for_stan(data, model$growth_measure)
+  data_for_stan <- prep_data_for_stan(data, model$growth_measure, model$crossval)
   stan(model_code = model$model_code,
        fit = model$fit,
        data = data_for_stan,
@@ -92,17 +149,18 @@ run_single_stan_chain <- function(model, data, chain_id, iter=4000,
        chain_id=chain_id,
        control =list(stepsize=0.1, adapt_delta=0.99, max_treedepth=15))
 }
-# Prepares data for models clusterous jobs
-prep_data_for_stan <- function(data, growth_measure) {
-  if (growth_measure == 'true_dbh_dt') {
-    growth_dt <- data$train$true_dbh_dt - 0.172
-    growth_dt_heldout = data$heldout[[growth_measure]] - 0.172
-  }
-  
-  if (growth_measure == 'true_basal_area_dt') {
-    growth_dt <- data$train$true_basal_area_dt - 0.338
-    growth_dt_heldout = data$heldout$true_basal_area_dt - 0.338
-  }
+
+# Prepares kfold data for use with stan
+prep_kfold_data_for_stan <- function(data, growth_measure) {
+  switch(growth_measure,
+         'true_dbh_dt' = {
+           growth_dt <- data$train$true_dbh_dt - 0.172
+           growth_dt_heldout = data$heldout[[growth_measure]] - 0.172
+         },
+         'true_basal_area_dt' = {
+           growth_dt <- data$train$true_basal_area_dt - 0.338
+           growth_dt_heldout = data$heldout$true_basal_area_dt - 0.338
+         })
   list(
     n_obs = nrow(data$train),
     n_census = max(data$train$censusid),
@@ -122,26 +180,47 @@ prep_data_for_stan <- function(data, growth_measure) {
     growth_dt_heldout = growth_dt_heldout,
     rho_c_heldout = unique(data$heldout$rho/0.6),
     y_heldout = as.integer(data$heldout$dead_next_census)
-    )
-}
-
-# Prepares data for 'best' model fitted to full dataset
-prep_full_data_for_stan <- function(data) {
-  list(
-    n_obs = nrow(data$train),
-    n_census = max(data$train$censusid),
-    n_spp = max(data$train$sp_id),
-    census = data$train$censusid,
-    spp = data$train$sp_id,
-    census_length = data$train$census_interval,
-    growth_dt = data$train$true_dbh_dt - 0.1,
-    rho_c  = unique(data$train$rho)/0.6,
-    y = as.integer(data$train$dead_next_census)
   )
 }
+
+# Prepares full dataset for use with stan
+prep_final_model_data <- function(data, growth_measure) {
+    switch(growth_measure,
+         'true_dbh_dt' = {
+           growth_dt <- data$train$true_dbh_dt - 0.172
+           growth_dt_heldout = data$heldout[[growth_measure]] - 0.172
+         },
+         'true_basal_area_dt' = {
+           growth_dt <- data$train$true_basal_area_dt - 0.338
+           growth_dt_heldout = data$heldout$true_basal_area_dt - 0.338
+         })
+  list(
+    n_obs = nrow(data),
+    n_census = max(data$censusid),
+    n_spp = max(data$sp_id),
+    census = data$censusid,
+    spp = data$sp_id,
+    census_length = data$census_interval,
+    growth_dt = growth_dt,
+    rho_c  = unique(data$rho)/0.6,
+    y = as.integer(data$dead_next_census)
+  )
+}
+
+# Wrapper function to call either kfold or full data for stan
+prep_data_for_stan <- function(data, growth_measure, crossval) {
+  if(crossval==TRUE) {
+    prep_kfold_data_for_stan(data, growth_measure)
+  }
+  else {
+    prep_full_data_for_stan(data, growth_measure)
+  }
+}
+
 # Builds the model code
 make_stan_model <- function(chunks) {
   list(
+    crossval = chunks$crossval,
     growth_measure = chunks$growth_measure,
     pars = chunks$pars,
     model_code = sprintf("
@@ -168,24 +247,39 @@ precompile <- function(task) {
   path <- precompile_model_path()
   model <- task$model
   ## Assemble the stan model:
-  if(model == "base_hazard") {
-    chunks <- get_model_chunks_base_haz(task)
-  }
-  if(model == "base_hazard_re") {
-    chunks <- get_model_chunks_base_haz_re(task)
-  }
-  if(model == "growth_hazard") {
-    chunks <- get_model_chunks_growth_haz(task)
-  }
-  if(model == "growth_hazard_re") {
-    chunks <- get_model_chunks_growth_haz_re(task)
-  }
-  if(model == "base_growth_hazard") {
-    chunks <- get_model_chunks_base_growth_haz(task)
-  }
-  if(model == "base_growth_hazard_re") {
-    chunks <- get_model_chunks_base_growth_haz_re(task)
-  }
+  switch(model, 
+         "base_hazard"= {
+           chunks <- get_model_chunks_base_haz(task)
+           chunks$crossval <- TRUE
+         },
+         "base_hazard_re"= {
+           chunks <- get_model_chunks_base_haz_re(task)
+           chunks$crossval <- TRUE
+         },
+         "growth_hazard"= {
+           chunks <- get_model_chunks_growth_haz(task)
+           chunks$crossval <- TRUE
+         },
+         "growth_hazard_re"= {
+           chunks <- get_model_chunks_growth_haz_re(task)
+           chunks$crossval <- TRUE
+         },
+         "base_growth_hazard"= {
+           chunks <- get_model_chunks_base_growth_haz(task)
+           chunks$crossval <- TRUE
+         },
+         "base_growth_hazard_re"= {
+           chunks <- get_model_chunks_base_growth_haz_re(task)
+           chunks$crossval <- TRUE
+         },
+         'final_model'= {
+           chunks <- get_final_model_chunks(task)
+           chunks$crossval <- FALSE
+         },
+         "final_base_growth_hazard_re"= {
+           chunks <- get_model_chunks_base_growth_haz_re(task, TRUE)
+           chunks$crossval <- FALSE
+         })
   model <- make_stan_model(chunks)
   sig <- digest::digest(model)
   fmt <- "%s/%s.%s"
