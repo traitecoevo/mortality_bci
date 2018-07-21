7b5199ab025aa8a9f43a12a78f2a801c5e4e84c4
James Camac <james.camac@gmail.com>
2018-07-22 00:09:53 +1000

    Fixed yaxis issue

--- R/plot_fig4.R
+++ R/plot_fig4.R
@@ -20,44 +20,47 @@ plot_fig4 <- function(model,
                         trait_name = "wood_density", 
                         hazard_curve, 
                         colour_by_trait = FALSE, 
-                        xlab= "", 
+                        xlab= NULL, 
                         ylab=NULL) +
-    theme(plot.margin=unit(c(0.35,0.2,0.1,0.5),"cm"))
+    theme(plot.margin=unit(c(0.35,0.2,0.5,0.5),"cm"))
   
   p2 <- plot_mu_curves(model, 
                        growth_range, 
                        trait_name = "wood_density", 
                        trait_values = c(0.2,0.8), 
                        hazard_curve, 
-                       xlab="", 
+                       xlab=NULL, 
                        ylab =NULL) +
     plot_theme() + 
-    theme(plot.margin=unit(c(0.35,0.2,0.1,0.5),"cm"))
+    theme(plot.margin=unit(c(0.35,0.2,0.5,0.5),"cm"))
 
   p3 <- plot_mu_curves(model,
                        growth_range, 
                        trait_name = "dbh_95", 
                        trait_values = c(3,180),
                        hazard_curve, 
-                       xlab= "", 
+                       xlab= NULL, 
                        ylab = NULL) +
         plot_theme() +
-        theme(plot.margin=unit(c(0.35,0.2,0.1,0.5),"cm"))
+        theme(plot.margin=unit(c(0.35,0.2,0.5,0.5),"cm"))
   
   p4 <- plot_mu_curves(model, 
                        growth_range, 
                        trait_name = "gap_index", 
                        trait_values = c(0.5, 0.8), 
                        hazard_curve, 
-                       xlab=expression("Annual dbh growth"~("cm yr"^-1)), 
+                       xlab= NULL, 
                        ylab = NULL) +
     plot_theme() +
-    theme(plot.margin=unit(c(0.35,0.2,0.1,0.5),"cm"))
+    theme(plot.margin=unit(c(0.35,0.2,0.5,0.5),"cm"))
   
   # ("1-yr mortality probability")
   cowplot::plot_grid(p1,p2,p3,p4, ncol=1, labels=c("A) Species curves",
                                                    "B) Wood density",
                                                    "C) Maximum dbh",
-                                                   "D) Light demand"), label_size = 7, label_fontface ="plain") +
-    draw_label("1-yr mortality probability", x= 0, y=0.5, vjust= 1.5, angle=90,size = 7)
+                                                   "D) Light demand"), 
+                     label_size = 7, 
+                     label_fontface ="plain") +
+    draw_label("Annual mortality probability", x= 0, y=0.5, vjust= 1.5, angle=90,size = 7) +
+    draw_label(expression("Annual dbh growth"~("cm yr"^-1)), x= 0.5, y=0.03, angle=0,size = 7)
 }
